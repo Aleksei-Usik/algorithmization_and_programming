f6bbc6e59f3559be779115a9c00a2765ec959574
Aleksei-Usik <145195635+Aleksei-Usik@users.noreply.github.com>
2023-10-05 13:37:49 +0300

    Update 2.cpp

--- contest_1/02/2.cpp
+++ contest_1/02/2.cpp
@@ -1,1 +1,16 @@
+package main
 
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	var top, dub int8
+	var chel float64
+	chel = 0.5 * 365
+	top = int8(math.Ceil(chel / 32))
+	dub = int8(math.Ceil(chel / 20))
+	fmt.Print(chel, top, dub)
+
+}
